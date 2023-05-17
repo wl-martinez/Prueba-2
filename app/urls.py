698657eaedf4d6c212daa1cbce4ccc812f5e99d9
wl-martinez <wl.martinez@duocuc.cl>
2023-05-17 01:26:30 -0400

    login terminado

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, login
+from .views import home, login, registro
 
 urlpatterns = [
     path('',home, name="home"),
-    path('login',login, name="login")
+    path('login',login, name="login"),
+    path('registro',registro, name="registro")
 ]
--- app/views.py
+++ app/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def login(request):
     return render(request,'app/login.html')
+
+def registro(request):
+    return render(request,'app/registro.html')

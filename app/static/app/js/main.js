de5eac11d39244c3b12135b0e5016967f41489db
wl-martinez <wl.martinez@duocuc.cl>
2023-05-17 07:59:43 -0400

    js

---
+++ app/static/app/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  
+  // Animation for sign up button
+  $(function() {
+    var effects = "animated pulse faster";
+    var effectsEnd =
+      "animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd";
+  
+    $("#kk").hover(function() {
+      $(this)
+        .addClass(effects)
+        .one(effectsEnd, function() {
+          $(this).removeclass(effects);
+        });
+    });
+  });
+    
+    // Function to get input value.
+    $("#but_sub").click(function() {
+      var first_value = $("#FirstName").val();
+      var last_value = $("#LastName").val();
+      var Email_value = $("#Email").val();
+      var Password_value = $("#Password").val();
+      var phone_value = $("#PhoneNumber").val();
+  
+     alert("First Name: " + first_value + "\nLast Name: " + last_value + "\nEmail: " + Email_value + "\nPassword: "+ Password_value + "\nPhone Number: " + phone_value);
+     $("#form")[0].reset();
+    });
+  });
+  

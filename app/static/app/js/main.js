d3ccb4fc404e8b52062ecd23ffc666ff24667484
wl-martinez <wl.martinez@duocuc.cl>
2023-05-24 22:47:43 -0400

    revision

--- app/static/app/js/main.js
+++ app/static/app/js/main.js
@@ -1,30 +1,31 @@
-$(document).ready(function() {
-  
-  // Animation for sign up button
-  $(function() {
+$(document).ready(function () {
+
+
+  $(function () {
     var effects = "animated pulse faster";
     var effectsEnd =
       "animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd";
-  
-    $("#kk").hover(function() {
+
+    $("#kk").hover(function () {
       $(this)
         .addClass(effects)
-        .one(effectsEnd, function() {
+        .one(effectsEnd, function () {
           $(this).removeclass(effects);
         });
     });
   });
-    
-    // Function to get input value.
-    $("#but_sub").click(function() {
-      var first_value = $("#FirstName").val();
-      var last_value = $("#LastName").val();
-      var Email_value = $("#Email").val();
-      var Password_value = $("#Password").val();
-      var phone_value = $("#PhoneNumber").val();
-  
-     alert("First Name: " + first_value + "\nLast Name: " + last_value + "\nEmail: " + Email_value + "\nPassword: "+ Password_value + "\nPhone Number: " + phone_value);
-     $("#form")[0].reset();
-    });
+
+
+  $("#but_sub").click(function () {
+    var first_value = $("#FirstName").val();
+    var last_value = $("#LastName").val();
+    var Email_value = $("#Email").val();
+    var Password_value = $("#Password").val();
+    var phone_value = $("#PhoneNumber").val();
+
+    alert("First Name: " + first_value + "\nLast Name: " + last_value + "\nEmail: " + Email_value + "\nPassword: " + Password_value + "\nPhone Number: " + phone_value);
+    $("#form")[0].reset();
   });
-  
+});
+
+
